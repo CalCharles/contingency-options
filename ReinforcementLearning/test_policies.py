d9f8b4de7fd8d46bff6cae9fa8380cfa01e0b06a
CalCharles <calebchuck@utexas.edu>
2019-04-26 13:54:27 -0500

    testing

--- ReinforcementLearning/test_policies.py
+++ ReinforcementLearning/test_policies.py
@@ -11,7 +11,7 @@ from Models.models import pytorch_model
 from ReinforcementLearning.rollouts import RolloutOptionStorage
 from file_management import save_to_pickle
 
-def testRL(args, save_path, true_environment, proxy_chain, proxy_environment, state_class, behavior_policy, reward_classes = None):
+def testRL(args, save_path, true_environment, proxy_chain, proxy_environment, state_class, behavior_policy, num_actions, reward_classes = None):
     print("#######")
     print("Evaluating Options")
     print("#######")
@@ -20,11 +20,12 @@ def testRL(args, save_path, true_environment, proxy_chain, proxy_environment, st
     base_env.set_save(0, args.save_dir, args.save_recycle)
     if reward_classes is not None:
         proxy_environment.reward_fns = reward_classes
+    args.changepoint_queue_len = max(args.changepoint_queue_len, args.num_iters)
     proxy_environment.initialize(args, proxy_chain, proxy_environment.reward_fns, proxy_environment.stateExtractor, behavior_policy)
     print(base_env.save_path)
-    behavior_policy.initialize(args, state_class.action_num)
+    behavior_policy.initialize(args, num_actions)
     train_models = proxy_environment.models
-    train_models.initialize(args, len(reward_classes), state_class)
+    train_models.initialize(args, len(reward_classes), state_class, num_actions)
     proxy_environment.duplicate(args)
     proxy_environment.set_save(0, args.save_dir, args.save_recycle)
     state = pytorch_model.wrap(proxy_environment.getState(), cuda = args.cuda)
@@ -34,9 +35,10 @@ def testRL(args, save_path, true_environment, proxy_chain, proxy_environment, st
     cs, cr = proxy_environment.getHistState()
     hist_state = pytorch_model.wrap(cs, cuda = args.cuda)
     cp_state = proxy_environment.changepoint_state([raw_state])
-    rollouts = RolloutOptionStorage(args.num_processes, (state_class.shape,), state_class.action_num, cr.flatten().shape[0], 
+    rollouts = RolloutOptionStorage(args.num_processes, (state_class.shape,), proxy_environment.action_size, cr.flatten().shape[0],
         state.shape, hist_state.shape, args.buffer_steps, args.changepoint_queue_len, args.trace_len, 
-        args.trace_queue_len, train_models.num_options, cp_state[0].shape, args.lag_num, args.cuda)
+        args.trace_queue_len, args.dilated_stack, args.target_stack, args.dilated_queue_len, train_models.currentOptionParam().shape[1:], len(train_models.models), cp_state[0].shape,
+        args.lag_num, args.cuda)
     option_actions = {option.name: collections.Counter() for option in train_models.models}
     total_duration = 0
     start = time.time()
@@ -45,10 +47,11 @@ def testRL(args, save_path, true_environment, proxy_chain, proxy_environment, st
     option_counter = collections.Counter()
     option_value = collections.Counter()
     raw_states = dict()
+    ep_reward = 0
     rollouts.set_parameters(args.num_iters * train_models.num_options)
-    if args.num_iters > rollouts.changepoint_queue_len:
-        rollouts.set_changepoint_queue(args.num_iters)
-
+    # if args.num_iters > rollouts.changepoint_queue_len:
+    #     rollouts.set_changepoint_queue(args.num_iters)
+    done = False
     for i in range(train_models.num_options):
         train_models.option_index = i
         train_models.currentModel().test = True
@@ -61,20 +64,23 @@ def testRL(args, save_path, true_environment, proxy_chain, proxy_environment, st
             values, dist_entropy, action_probs, Q_vals = train_models.determine_action(current_state.unsqueeze(0), current_resp.unsqueeze(0))
             v, ap, qv = train_models.get_action(values, action_probs, Q_vals)
             cp_state = proxy_environment.changepoint_state([raw_state])
+            ep_reward += base_env.reward
             # print(ap, qv)
             action = behavior_policy.take_action(ap, qv)
-            rollouts.insert(j, state, current_state, action_probs, action, Q_vals, values, train_models.option_index, cp_state[0], pytorch_model.wrap(args.greedy_epsilon, cuda=args.cuda), current_resp)
+            rollouts.insert(False, state, current_state, pytorch_model.wrap(args.greedy_epsilon, cuda=args.cuda), done, current_resp, action, cp_state[0], train_models.currentOptionParam(), train_models.option_index, None, None, action_probs, Q_vals, values)
             state, raw_state, resp, done, action_list = proxy_environment.step(action, model = False)#, render=len(args.record_rollouts) != 0, save_path=args.record_rollouts, itr=fcnt)
             raw_states[train_models.currentName()].append(raw_state)
             option_actions[train_models.currentName()][int(pytorch_model.unwrap(action.squeeze()))] += 1
             if done:
-                pass
+                print("Episode Reward: ", ep_reward, " ", fcnt)
+                ep_reward = 0
                 # print("reached end")
-            proxy_environment.determine_swaps(length, needs_rewards=True) # doesn't need to generate rewards
+            # proxy_environment.determine_swaps(length, needs_rewards=True) # doesn't need to generate rewards
 
         print(args.num_iters)
         print(action_probs)
-        rewards = proxy_environment.computeReward(rollouts, args.num_iters)
+        print("Episode Reward: ", ep_reward, " ", fcnt)
+        rewards = proxy_environment.computeReward(args.num_iters)
         # print(rewards.shape)
         print(rewards.sum())
         rollouts.insert_rewards(rewards, total_duration)
